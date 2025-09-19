9353f1ec37e926a44b7e9bf53525ded0f283a408
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-09-19 10:34:17 +0200

    Update copyright notice
    Update copyright notice

--- src/main/java/io/axoniq/framework/postgresql/Placeholder.java
+++ src/main/java/io/axoniq/framework/postgresql/Placeholder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2025-2025. AxonIQ B.V.
+ *
+ * Licensed under the AXONIQ SOFTWARE SUBSCRIPTION AGREEMENT TERMS,
+ * Version September 2025 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ *    https://lp.axoniq.io/axoniq-software-subscription-agreement-terms
+ *
+ * The software is available under Non-Production Free License.
+ * Production use requires a paid license. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.
+ */
+
 package io.axoniq.framework.postgresql;
 
 public class Placeholder {
